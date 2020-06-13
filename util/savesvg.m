158ad6aa1853f549d7bcb4ede6e11ba789314147
david <dk.shin1992@gmail.com>
2020-06-13 13:56:27 +1000

    create wrapper to matlab's default fig export in svg

---
+++ util/savesvg.m
@@ -0,0 +1,17 @@
+function savesvg(hfig,filename)
+% Save figures as SVG using MATLAB's default function.
+% DKS 2020
+
+timestr = getdatetimestr;
+if nargin < 1
+    hfig = gcf;
+end
+if ~exist('filename','var')
+    filename = hfig.Name;
+end
+if isempty(filename)
+    filename = 'untitled';
+end
+foutname = strjoin({filename,timestr},'_');
+saveas(hfig,foutname,'svg');
+end
